a87df7c253969a148ad03aecb8b01379177d3541
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-11 16:54:57 +0200

    feat: add textarea and submit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { useChat } from '@ai-sdk/react'
 import { Input } from '@codegouvfr/react-dsfr/Input'
+import { Button } from '@codegouvfr/react-dsfr/Button'
 import { Box, Container } from '@mui/material'
 
 export default function Page() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: 'http://localhost:8000/chat'
+  })
+
   return (
     <Container>
       <Box
@@ -12,11 +20,17 @@ export default function Page() {
           height: '100vh'
         }}
       >
-        <Input
-          label="Comment puis-je vous aider ?"
-          textArea
-          style={{ width: '100%' }}
-        />
+        <form onSubmit={handleSubmit} style={{ flex: 1 }}>
+          <Input
+            label="Comment puis-je vous aider ?"
+            nativeTextAreaProps={{
+              value: input,
+              onChange: handleInputChange
+            }}
+            textArea
+          />
+          <Button type="submit">Envoyer</Button>
+        </form>
       </Box>
     </Container>
   )
